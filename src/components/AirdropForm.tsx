8f903b2fa5febfbdd0458aefdc68e0f95a6e302e
b-elia <bordonielia96@gmail.com>
2025-06-04 13:49:42 +0200

    keep working on form

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -1,14 +1,44 @@
 "use client";
 import InputField from "@/components/UI/InputField";
 import { useState } from "react";
+import { chainsToTSender, tsenderAbi, erc20Abi } from "@/constants";
+import { useChainId, useConfig, useAccount } from "wagmi";
+import { readContract } from "@wagmi/core";
+
+type addressType = `0x${string}`;
 
 export default function AirdropForm() {
   const [tokenAddress, setTokenAddress] = useState("");
   const [recepients, setRecepients] = useState("");
   const [amounts, setAmounts] = useState("");
 
+  const chainId = useChainId();
+  const config = useConfig();
+  const account = useAccount();
+
+  async function getApprovedAmount(tSenderAddress: string | null): Promise<number> {
+    if (!tSenderAddress) {
+      alert("TSender address not found for this chain.");
+      return 0;
+    }
+
+    const response = await readContract(config, {
+      abi: erc20Abi,
+      address: tokenAddress as addressType,
+      functionName: "allowance",
+      args: [account.address, tSenderAddress as addressType],
+    });
+
+    return response as number;
+  }
+
   async function handleSubmit() {
-    console.log("submitting airdrop");
+    // 1a. If already approved, move to step 2
+    // 1b. If not approved, Approve tsender contract to send our token
+    // 2. Call the airdrop function on the tsender contract
+    // 3. Wait for the transaction to be mined
+    const tSenderAddress = chainsToTSender[chainId]["tsender"];
+    const approvedAmount = await getApprovedAmount(tSenderAddress);
   }
 
   return (
@@ -28,7 +58,12 @@ export default function AirdropForm() {
         large={true}
         onChange={(e) => setAmounts(e.target.value)}
       />
-      <button onClick={handleSubmit}>Send Tokens</button>
+      <button
+        onClick={handleSubmit}
+        className="flex items-center justify-center bg-gradient-to-r from-orange-500 to-amber-500 hover:from-orange-600 hover:to-amber-600 text-white font-bold py-3 px-6 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1 focus:outline-none focus:ring-4 focus:ring-orange-300 focus:ring-opacity-50"
+      >
+        Send Tokens
+      </button>
     </div>
   );
 }
